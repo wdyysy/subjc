6f949ba50aac19af384abd5945732e526bc6d8be
westonfree <12569779+wdyysy@users.noreply.github.com>
2026-03-03 15:44:47 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-          "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
+          "自用后端【vless reality+hy1+hy2】": "https://cl.ll2hhh.filegear-sg.me",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://api.v1.mk",
@@ -1304,3 +1304,4 @@ export default {
   }
 };
 </script>
+
