cc3c49470286d1e5012f5eafcde690f3ee2745cf
westonfree <12569779+wdyysy@users.noreply.github.com>
2026-03-12 21:13:55 +0800

    更新 Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -1293,10 +1293,10 @@ export default {
         .then(res => {
           this.backendVersion = res.data.replace(/backend\n$/gm, "");
           this.backendVersion = this.backendVersion.replace("subconverter", "SubConverter");
-          this.backendVersion += " version返回后加上语句-除肥羊外的后端";
+          this.backendVersion += " version返回后自加文字";
           let a = this.form.customBackend.indexOf("url.v1.mk") !== -1 || this.form.customBackend.indexOf("api.v1.mk") !== -1;
           let b = this.form.customBackend.indexOf("127.0.0.1") !== -1;
-          a ? this.$message.success(`${this.backendVersion}` + "肥羊负载均衡增强版后端，已屏蔽免费节点池（会返回403），额外支持vless reality+hysteria+hysteria2订阅转换") : b ? this.$message.success(`${this.backendVersion}` + "本地局域网自建版后端") : this.$message.success(`${this.backendVersion}` + "---这句是后面succe第三个语句");
+          a ? this.$message.success(`${this.backendVersion}` + "肥羊负载均衡增强版后端，已屏蔽免费节点池（会返回403），额外支持vless reality+hysteria+hysteria2订阅转换") : b ? this.$message.success(`${this.backendVersion}` + "本地局域网自建版后端") : this.$message.success(`${this.backendVersion}` + "---这句是后语句");
         })
         .catch(() => {
           this.$message.error("请求SubConverter版本号返回数据失败，该后端不可用！");
